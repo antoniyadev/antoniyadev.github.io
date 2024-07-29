2d42b292eb6b8233a4d10ae3a2ebf7d93aefb26b
Antoniya <st.antoniya@gmail.com>
2024-07-29 11:03:12 +0300

    change image sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -60,7 +60,7 @@ module.exports = {
         },
       },
       animation: {
-        typing: "typing 2s steps(10), blink .5s step-end infinite",
+        typing: "typing 2s steps(20), blink .5s step-end infinite",
         moveInLeft: "moveInLeft 2s ease-in .5s",
         moveInBottom: "moveInBottom .5s ease-out",
       },
